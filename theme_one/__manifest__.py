0860ee43d19a2697ac8ca70da9657a21c66c30be
OGGonzalezM <ogm@soluciones4g.com>
2017-06-30 12:42:15 -0500

    [ADD] theme_one: Adding the first theme

---
+++ theme_one/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "First theme",
+
+    'summary': """
+        First module created""",
+
+    'description': """
+        This is the first theme created by Soluciones4g
+    """,
+
+    'author': "OGGonzalezM",
+    'website': "http://www.soluciones4g.com",
+
+    # Categories can be used to filter modules in modules listing
+    # for the full list
+    'category': 'Theme/Creative',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['website'],
+
+    # always loaded
+    'data': [
+        'views/layout.xml',
+        'views/pages.xml',
+        'views/assets.xml',
+    ],
+    
+    'installable':True,
+    'auto_install':False,
+}
